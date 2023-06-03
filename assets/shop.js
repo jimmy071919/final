3a7cabc01137134100706415335d151b161e22a7
jimmy071919 <jimmy071919@gmail.com>
2023-06-03 12:33:21 +0800

    修改

--- assets/shop.js
+++ assets/shop.js
@@ -5,8 +5,8 @@ window.onload = function() {
 // 用以確保網頁中的內容都跑完了才呈現出來
 
 const shopitem1 = {
-    0: {id:'0',name: '酒一', description: '樂事只選用當令新鮮馬鈴薯', price: '25', img: 'wine.png'},
-    1: {id:'1',name: '酒二', description: '維力炸醬麵，麵體Q、料香濃，乾麵附湯一包兩吃', price: '20', img: 'wine.png'},
+    0: {id:'0',name: '龍舌蘭', description: '樂事只選用當令新鮮馬鈴薯', price: '25', img: 'tequila.jpg'},
+    1: {id:'1',name: '伏特加', description: '維力炸醬麵，麵體Q、料香濃，乾麵附湯一包兩吃', price: '20', img: 'wine.png'},
     2: {id:'2',name: '酒三', description: '遵循傳統焙炒工藝成就特有大麥風味溫潤茶韻與順', price: '15', img: 'wine.png'},
     3: {id:'3',name: '酒四', description: '醇厚紅茶結合焙炒大麥，成就獨特大麥風味、暢銷全台灣的紅茶飲料。', price: '15', img: 'wine.png'},
     4: {id:'4',name: '酒五', description: '選用甜香特色的焙香綠茶葉，萃取醇厚甜香茶汁，呈現獨特大麥風味的綠茶。', price: '15', img: 'wine.png'},
@@ -41,8 +41,7 @@ function newCardItem(item) {
     temp+='<div>';
     temp+=  '<div class="production"><img src="./image/'+item.img+'"></img>';
     temp+=  '<div class="name">'+item.name+'</div>'
-    temp+=  '<div class="name">'+item.description+'</div>'
-    temp+=  '<p><a class="moreinfo" href="./shop2.html?id=' + item.id + '">查看詳情</a></div></p>';
+    temp+=  '<p><a class="moreinfo" target="_blank" href="./shop2.html?id=' + item.id + '">查看詳情</a></div></p>';
     temp+='</div>';
     // 這邊會呼應到上面的generateItems的函數
     return temp;
