1f0413ec1ef4c23aedc3c19b39db448c3db0c2b8
jimmy071919 <jimmy071919@gmail.com>
2023-06-04 01:47:38 +0800

    修改

--- assets/shop.js
+++ assets/shop.js
@@ -6,25 +6,28 @@ window.onload = function() {
 // 用以確保網頁中的內容都跑完了才呈現出來
 
 const shopitem1 = {
-    0: {id:'0',name: '龍舌蘭', description: '樂事只選用當令新鮮馬鈴薯', price: '25', img: 'tequila.jpg'},
-    1: {id:'1',name: '伏特加', description: '維力炸醬麵，麵體Q、料香濃，乾麵附湯一包兩吃', price: '20', img: 'wine.png'},
-    2: {id:'2',name: '酒三', description: '遵循傳統焙炒工藝成就特有大麥風味溫潤茶韻與順', price: '15', img: 'wine.png'},
-    3: {id:'3',name: '酒四', description: '醇厚紅茶結合焙炒大麥，成就獨特大麥風味、暢銷全台灣的紅茶飲料。', price: '15', img: 'wine.png'},
-    4: {id:'4',name: '酒五', description: '選用甜香特色的焙香綠茶葉，萃取醇厚甜香茶汁，呈現獨特大麥風味的綠茶。', price: '15', img: 'wine.png'},
+    0: {id:'0',name: '白酒',  price: '25', img: 'spirit.jpg'},
+    1: {id:'1',name: '白蘭地',  price: '20', img: 'BR.jpg'},
+    2: {id:'2',name: '威士忌',  price: '15', img: 'wis.png'},
+    3: {id:'3',name: '伏特加', price: '15', img: 'vodka_2.png'},
+    4: {id:'4',name: '蘭姆酒', price: '15', img: 'lan_2.jpg'},
+    5: {id:'5',name: '苦艾酒', price: '15', img: 'absinthe.jpg'},
 };
 const shopitem2 = {
-    0: {id:'5',name: '酒六', description: '樂事只選用當令新鮮馬鈴薯，鮮切成金黃香脆洋芋片', price: '25', img: 'wine.png'},
-    1: {id:'6',name: '酒七', description: '維力炸醬麵，麵體Q、料香濃', price: '20', img: 'wine.png'},
-    2: {id:'7',name: '酒八', description: '遵循傳統焙炒工藝成就特有大麥茶。', price: '15', img: 'wine.png'},
-    3: {id:'8',name: '酒酒', description: '醇厚紅茶結合焙炒大麥，', price: '15', img: 'wine.png'},
-    4: {id:'9',name: '酒十', description: '選用甜香特色的焙香綠茶葉', price: '15', img: 'wine.png'},
+    0: {id:'6',name: '日本清酒', price: '25', img: 'sake.jpg'},
+    1: {id:'7',name: '奶酒', price: '20', img: 'milkwine.jpg'},
+    2: {id:'8',name: '草莓酒',  price: '15', img: 'strwine.jpg'},
+    3: {id:'9',name: '葡萄酒',  price: '15', img: 'gw.jpg'},
+    4: {id:'10',name: '米酒' , price: '15', img: 'ricewine.png'},
+    5: {id:'11',name: '梅酒',  price: '15', img: 'plumwine.jpg'},
+    6: {id:'12',name: 'liz酒',  price: '15', img: 'liz_2.jpg'},
 };
 const shopitem3 = {
-    0: {id:'5',name: '酒六', description: '樂事只選用當令新鮮馬鈴薯，鮮切成金黃香脆洋芋片', price: '25', img: 'wine.png'},
-    1: {id:'6',name: '酒七', description: '維力炸醬麵，麵體Q、料香濃', price: '20', img: 'wine.png'},
-    2: {id:'7',name: '酒八', description: '遵循傳統焙炒工藝成就特有大麥茶。', price: '15', img: 'wine.png'},
-    3: {id:'8',name: '酒酒', description: '醇厚紅茶結合焙炒大麥，', price: '15', img: 'wine.png'},
-    4: {id:'9',name: '酒十', description: '選用甜香特色的焙香綠茶葉', price: '15', img: 'wine.png'},
+    0: {id:'13',name: '香檳酒',  price: '25', img: 'sp.png'},
+    1: {id:'14',name: '氣泡酒',  price: '20', img: 'chepo.jpg'},
+    2: {id:'15',name: '女巫酒',  price: '15', img: 'witchwine.png'},
+    3: {id:'16',name: '琴酒',  price: '15', img: 'jin.jpg'},
+    4: {id:'17',name: '常常酒酒',  price: '15', img: 'liz.jpg'},
 };
 function generateItems() {
     let objLength = Object.keys(shopitem1).length;
